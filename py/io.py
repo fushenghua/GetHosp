acc1c73bfcaded2b4979f957320e3e3a8b63b225
git <fushenghua2012@126.com>
2016-05-07 01:39:11 +0800

    update  host database

--- py/io.py
+++ py/io.py
@@ -1,16 +1,33 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 import os
-fp=open("io.txt", "r");
-for eachline in fp:
-    fobj = open("2016-05-03-"+eachline.rstrip()+".markdown", 'w')
-    fobj.writelines("--- \n")
-    fobj.writelines("layout: post \n")
-    fobj.writelines("title: "+eachline+"\n")
-    fobj.writelines("date:   2016-05-03 13:39:56 \n")
-    fobj.writelines("categories: hosp \n")
-    fobj.writelines("--- \n")
-    fobj.writelines("   \n")
-    fobj.writelines(eachline)
-    fobj.close()
-    print eachline,;
+
+
+def createMDForFile():
+    fp=open("io.txt", "r");
+    for eachline in fp:
+        fobj = open("2016-05-03-"+eachline.rstrip()+".markdown", 'w')
+        fobj.writelines("--- \n")
+        fobj.writelines("layout: post \n")
+        fobj.writelines("title: "+eachline+"\n")
+        fobj.writelines("date:   2016-05-03 13:39:56 \n")
+        fobj.writelines("categories: hosp \n")
+        fobj.writelines("--- \n")
+        fobj.writelines("   \n")
+        fobj.writelines(eachline)
+        fobj.close()
+        print eachline,;
+
+def createMD(title,cate,city,info):
+        fobj = open("../_posts/2016-05-03-"+title.rstrip()+".markdown", 'w')
+        fobj.writelines("--- \n")
+        fobj.writelines("layout: post \n")
+        fobj.writelines("title: "+title+"\n")
+        fobj.writelines("date:   2016-05-03 13:39:56 \n")
+        fobj.writelines("categories:"+cate+"  \n")
+        fobj.writelines("city:"+city+"  \n")
+        fobj.writelines("--- \n")
+        fobj.writelines("   \n")
+        fobj.writelines(title+"\n")
+        fobj.writelines(info+"\n")
+        fobj.close()
